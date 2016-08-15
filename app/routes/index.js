062a455c19a46190272d0ca872e2c7dfceef9293
Shradha Pulla <pullashradha@gmail.com>
2016-08-15 16:02:21 -0700

    Create delete rental functionality

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll("rental");
+  },
+  actions: {
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo("index");
+    }
   }
 });
